c740cf51e027f179a5298586e145a0daa515bb05
a1iya <aliyusha.gizatullina@gmail.com>
2019-01-17 17:25:20 +0100

    mairies

--- lib/mairies.rb
+++ lib/mairies.rb
@@ -1,31 +1,21 @@
-=begin require 'Nokogiri'
-require 'rubygems'
-require 'open-uri'
-
-page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
-
-villes_array = page.xpath('//*[@class="lientxt"]')
-villes = []
-
-villes_array.each do |villes_array|
-    villes << villes_array.text
-end
-
-print villes_array
-
-//*[@id="voyance-par-telephone"]/table/tbody/tr[2]/td/table/tbody/tr/td[2]/p/a[1]
-=end
-
 require 'nokogiri'
 require 'rubygems'
 require 'open-uri'
-def get_townhall_urls
-  townhall_urls = []
-  page = Nokogiri::HTML(open("http://www.annuaire-des-mairies.com/val-d-oise.html"))
-  page.xpath('//p/a').each do |url|
-  townhall_urls = url.text
-puts townhall_urls
-#  townhall_urls_complete = townhall_urls.map{|i| i.downcase}
-  end
+townhall=[]
+page = Nokogiri::HTML(open("http://www.annuaire-des-mairies.com/val-d-oise.html"))
+page.xpath('//p/a').each do |name|
+    townhall << name.text
 end
-get_townhall_urls
+email = []
+page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/95/"))
+news_links = page.css("a")
+news_links.each do |link|
+    page_annuaire = Nokogiri::HTML(open("http://annuaire-des-mairies.com/95/#{link['href']}"))
+    page_annuaire.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').each do |mail|
+        email << mail.text
+    end
+end
+hash = Hash[townhall.zip(email.map)]
+a = []
+hash.each {|index| a << {index[0] => index[1]}}
+puts a
