4241124c277399a9af53b839565bd24404f094cf
jordan aumis <45138539+jordan-aumis@users.noreply.github.com>
2019-01-17 23:28:14 +0100

    Update 01_mairies.rb

--- lib/01_mairies.rb
+++ lib/01_mairies.rb
@@ -1,21 +1,36 @@
 require 'nokogiri'
 require 'rubygems'
 require 'open-uri'
-townhall=[]
-page = Nokogiri::HTML(open("http://www.annuaire-des-mairies.com/val-d-oise.html"))
-page.xpath('//p/a').each do |name|
-    townhall << name.text
+require 'pry'
+
+
+#Récupération d'une array avec la liste des villes
+def get_townhall_urls
+  page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
+  page.xpath('//a[@class = "lientxt"]').map { |town|
+  town.text }
 end
-email = []
-page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/95/"))
-news_links = page.css("a")
-news_links.each do |link|
-    page_annuaire = Nokogiri::HTML(open("http://annuaire-des-mairies.com/95/#{link['href']}"))
-    page_annuaire.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').each do |mail|
-        email << mail.text
-    end
+
+#Récupération d'une array avec la liste des emails associés à chaque ville
+def get_townhall_email
+  page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/95/"))
+  news_links = page.css("a")
+  news_links.map { |link|
+  page_annuaire = Nokogiri::HTML(open("http://annuaire-des-mairies.com/95/#{link['href']}"))
+  page_annuaire.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').map { |mail|
+  mail.text }}
 end
-hash = Hash[townhall.zip(email.map)]
-a = []
-hash.each {|index| a << {index[0] => index[1]}}
-puts a
+
+# Fusion des 2 arrays dans une hash
+def megahash
+megarray = get_townhall_urls.zip(get_townhall_email.flatten)
+
+final_result_hash_a = megarray.map { |town, email| Hash["#{town}": email] }
+
+end
+
+def perform
+    megahash
+end
+
+puts perform
