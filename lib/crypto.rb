84b9924de0d690cc256ed7cc0bd1f55a5f7e9256
a1iya <aliyusha.gizatullina@gmail.com>
2019-01-17 12:25:41 +0100

    crypto

---
+++ lib/crypto.rb
@@ -0,0 +1,32 @@
+require 'Nokogiri'
+require 'rubygems'
+require 'open-uri'
+
+value = []
+currency = []
+
+page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
+
+
+page.xpath('//a[@class = "link-secondary"]').each do |devise|
+
+    currency << devise.text
+
+end
+
+# print currency
+
+
+page.xpath('//a[@class = "price"]').each do |valeur|
+
+    value << valeur.text
+
+end
+
+# print value
+
+values = value.map{|e| e.delete('$').to_f }
+
+my_hash = Hash[currency.zip(values.map)]
+
+print my_hash
